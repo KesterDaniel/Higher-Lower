28f032bdb57c838a88d5f2e8bc8ec8d49d5da14f
KesterDaniel <kesterdan17@gmail.com>
2023-01-10 17:16:05 +0100

    getting user to decide

--- app.py
+++ app.py
@@ -15,5 +15,10 @@ while pick2 == pick1:
 
 celeb_B = pick2
 
+while end == False:
+  print(f"A: {celeb_A['name']}, {celeb_A['description']} from {celeb_A['country']}")
+  print("                     VS")
+  print(f"B: {celeb_B['name']}, {celeb_B['description']} from {celeb_B['country']}")
+  user_pick = input("Who has more followers?: ")
 
 
