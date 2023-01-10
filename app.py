fa5990dedc9df9708b56c2b7110db4a4ab351d5b
KesterDaniel <kesterdan17@gmail.com>
2023-01-10 17:09:27 +0100

    pick random celebrities for celebA and celebB

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # Higher lower game
+# import pprint
+import random
+from data import data
+
+score = 0
+end = False
+
+pick1 = random.choice(data)
+pick2 = random.choice(data)
+celeb_A = pick1
+
+while pick2 == pick1:
+  pick2 = random.choice(data)
+
+celeb_B = pick2
+
+
+
