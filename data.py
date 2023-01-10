d63088ec9c77daa671205f2ae7e81d46249c71d0
KesterDaniel <kesterdan17@gmail.com>
2023-01-10 16:50:18 +0100

    added data file

---
+++ data.py
@@ -0,0 +1,86 @@
+data = [
+  {
+    "name": "Instagram",
+    "follower_count": 348,
+    "description": "Social media platform",
+    "country": "United States"
+  },
+  {
+    "name": "Cristiano Ronaldo",
+    "follower_count": 215,
+    "description": "Footballer",
+    "country": "Portugal"
+  },
+  {
+    "name": "Ariana Grande",
+    "follower_count": 185,
+    "description": "Footballer",
+    "country": "Portugal"
+  },
+  {
+    "name": "Dwayne Johnson",
+    "follower_count": 110,
+    "description": "Actor",
+    "country": "America"
+  },
+  {
+    "name": "Meek Mill",
+    "follower_count": 200,
+    "description": "Musician",
+    "country": "America"
+  },
+  {
+    "name": "Lionel Messi",
+    "follower_count": 300,
+    "description": "Footballer",
+    "country": "Argentina"
+  },
+  {
+    "name": "Angelina Jolie",
+    "follower_count": 192,
+    "description": "Actress",
+    "country": "England"
+  },
+  {
+    "name": "Jennifer Lopez",
+    "follower_count": 230,
+    "description": "Actress/Singer",
+    "country": "America"
+  },
+  {
+    "name": "Toyin Abraham",
+    "follower_count": 3,
+    "description": "Actress",
+    "country": "Nigeria"
+  },
+  {
+    "name": "Ayodeji Balogun",
+    "follower_count": 20,
+    "description": "Musician",
+    "country": "Nigeria"
+  },
+  {
+    "name": "Kylian Mbappe",
+    "follower_count": 190,
+    "description": "Footballer",
+    "country": "France"
+  },
+  {
+    "name": "David Adeleke",
+    "follower_count": 21,
+    "description": "Musician",
+    "country": "Nigerian"
+  },
+  {
+    "name": "Kevin Hart",
+    "follower_count": 115,
+    "description": "Actor/Comedian",
+    "country": "America"
+  },
+  {
+    "name": "Jaden Smith",
+    "follower_count": 100,
+    "description": "Musician",
+    "country": "America"
+  }
+]
